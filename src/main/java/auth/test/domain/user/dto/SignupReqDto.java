d52c1b09773ba467171366fd7f55a63bd1559d3a
jiyeon0926 <yeoni9094@naver.com>
2025-05-13 19:18:59 +0900

    fix: SignupReqDto 가변 객체로 수정

--- src/main/java/auth/test/domain/user/dto/SignupReqDto.java
+++ src/main/java/auth/test/domain/user/dto/SignupReqDto.java
@@ -1,12 +1,10 @@
 package auth.test.domain.user.dto;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class SignupReqDto {
 
-    private final String email;
-    private final String password;
+    private String email;
+    private String password;
 }
