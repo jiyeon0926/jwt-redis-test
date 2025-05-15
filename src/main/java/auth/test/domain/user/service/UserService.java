f67ef11b42dc1fc4cdf49dcb58aff91fbea008ac
jiyeon0926 <yeoni9094@naver.com>
2025-05-15 15:05:43 +0900

    refactor: 이메일 중복 예외처리

--- src/main/java/auth/test/domain/user/service/UserService.java
+++ src/main/java/auth/test/domain/user/service/UserService.java
@@ -3,9 +3,11 @@ package auth.test.domain.user.service;
 import auth.test.domain.user.entity.User;
 import auth.test.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,11 @@ public class UserService {
 
     @Transactional
     public void signup(String email, String password) {
+        userRepository.findByEmail(email)
+                .ifPresent(user -> {
+                    throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+                });
+
         String encodedPassword = passwordEncoder.encode(password);
 
         User user = new User(email, encodedPassword);
